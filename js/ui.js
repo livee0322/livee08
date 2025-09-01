5675b1b27081ccd5072cac01eab0d7627677c424
livee0322 <dlxodnd6564@naver.com>
2025-09-01 18:40:17 +0900

    ui.js 업데이트

--- js/ui.js
+++ js/ui.js
@@ -1,44 +1,47 @@
-// 상단/하단 공통 UI 모듈
 (() => {
-  const $ = (s, d=document) => d.querySelector(s);
-
-  // Appbar
-  $('#appbar').innerHTML = `
-    <div class="lv-appbar">
-      <div class="lv-appbar__in">
-        <div class="lv-appbar__title">라이비</div>
-        <div class="lv-appbar__icons">
-          <button class="lv-ic" aria-label="알림">🔔</button>
-          <button class="lv-ic" aria-label="검색">🔎</button>
-          <button class="lv-ic" aria-label="로그아웃">↩︎</button>
+  // 상단 앱바
+  const appbar = document.getElementById('appbar');
+  if (appbar) {
+    appbar.innerHTML = `
+      <div class="appbar">
+        <div class="appbar__in">
+          <div class="appbar__title">라이비</div>
+          <div class="appbar__actions">
+            <button class="ic-btn" aria-label="알림">🔔</button>
+            <button class="ic-btn" aria-label="검색">🔍</button>
+            <button class="ic-btn" aria-label="로그아웃">↩︎</button>
+          </div>
         </div>
-      </div>
-    </div>
-  `;
+      </div>`;
+  }
 
-  // Top tabs
-  $('#top-tabs').innerHTML = `
-    <div class="lv-topTabs">
-      <div class="lv-topTabs__in">
-        <a class="lv-tab is-active" href="#/home">숏클립</a>
-        <a class="lv-tab" href="#/live">쇼핑라이브</a>
-        <a class="lv-tab" href="#/news">뉴스</a>
-        <a class="lv-tab" href="#/event">이벤트</a>
-        <a class="lv-tab" href="#/service">서비스</a>
-      </div>
-    </div>
-  `;
+  // 상단 탭
+  const topTabs = document.getElementById('top-tabs');
+  if (topTabs) {
+    topTabs.innerHTML = `
+      <div class="topTabs">
+        <div class="topTabs__in">
+          <a class="tab is-active" href="#/home">숏클립</a>
+          <a class="tab" href="#/live">쇼핑라이브</a>
+          <a class="tab" href="#/news">뉴스</a>
+          <a class="tab" href="#/event">이벤트</a>
+          <a class="tab" href="#/service">서비스</a>
+        </div>
+      </div>`;
+  }
 
-  // Bottom tabbar
-  $('#tabbar').innerHTML = `
-    <div class="lv-tabbar">
-      <div class="lv-tabbar__in">
-        <a class="lv-tbi is-active" href="#/home">홈</a>
-        <a class="lv-tbi" href="recruit-new.html">모집캠페인</a>
-        <a class="lv-tbi" href="#/library">라이브러리</a>
-        <a class="lv-tbi" href="#/portfolios">인플루언서</a>
-        <a class="lv-tbi" href="login.html">마이페이지</a>
-      </div>
-    </div>
-  `;
+  // 하단 탭
+  const bottom = document.getElementById('bottom-tabs');
+  if (bottom) {
+    bottom.innerHTML = `
+      <div class="tabbar">
+        <div class="tabbar__in">
+          <a class="tbi is-active" href="#/home"><i>🏠</i><span>홈</span></a>
+          <a class="tbi" href="recruit-new.html"><i>🗂️</i><span>모집캠페인</span></a>
+          <a class="tbi" href="#/library"><i>🔖</i><span>라이브러리</span></a>
+          <a class="tbi" href="#/portfolios"><i>👤</i><span>인플루언서</span></a>
+          <a class="tbi" href="login.html"><i>⚙️</i><span>마이페이지</span></a>
+        </div>
+      </div>`;
+  }
 })();
